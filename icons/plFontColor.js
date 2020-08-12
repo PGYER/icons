76534394eef347837ed8396a4898e6f189d8bcca
Carney Wu <carneywu@pgyer.com>
2020-08-12 16:36:15 +0800

    add some icons

---
+++ icons/plFontColor.js
@@ -0,0 +1,7 @@
+// plFont
+const name = 'font'
+const size = [1024, 1024]
+const path0 = 'M620,0l327.3,1024H791l-86.1-280.5h-390L227.1,1024H76.7L401,0L620,0L620,0z M354.1,616.4h311.1l-87.7-284.8 c-17.5-62.9-57-189.9-65.8-229.4h-5.9c-7.3,38-46.8,168-62.9,225L354.1,616.4z'
+const path1 = ''
+
+export default [name, size, path0, path1]
